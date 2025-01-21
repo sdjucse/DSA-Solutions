b28020b70ae42709006921d212c702935dfc8dd6
Sarthak Dubey <sarthak.dubey1208@gmail.com>
2025-01-21 20:29:50 +0530

    Create RotateImage.java

---
+++ Arrays/RotateImage.java
@@ -0,0 +1,34 @@
+package Arrays;
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                // Swap matrix[i][j] with matrix[j][i]
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            int left = 0;
+            int right = n - 1;
+
+            // Reverse the row using two pointers
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+    }
+
+    }
