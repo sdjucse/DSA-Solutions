292324ae3518454bda62e2fb739bf6ffcb37d258
Sarthak Dubey <sarthak.dubey1208@gmail.com>
2025-01-28 19:47:04 +0530

    Create MaximizeHappinessOfChildren.java

---
+++ Greedy/MaximizeHappinessOfChildren.java
@@ -0,0 +1,25 @@
+class Solution {
+    public long maximumHappinessSum(int[] happiness, int k) {
+        Arrays.sort(happiness);
+        reverseArray(happiness);
+        long maxi=0;
+        int cnt=0;
+        for(int i=0;i<k;i++){
+            maxi+=Math.max(happiness[i]-cnt,0);
+            cnt++;
+        }
+        return maxi;
+    }
+    private void reverseArray(int[] happiness){
+        int n=happiness.length;
+        int i=0;
+        int j=n-1;
+        while(i<j){
+            int temp=happiness[i];
+            happiness[i]=happiness[j];
+            happiness[j]=temp;
+            i++;
+            j--;
+        }
+    }
+}
