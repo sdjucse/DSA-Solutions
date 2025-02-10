f675e97dd511428544f4cc894816113fe9d9790b
Sarthak Dubey <sarthak.dubey1208@gmail.com>
2025-02-10 14:22:45 +0530

    Create sumofstringnums.java

---
+++ gfgpotd/sumofstringnums.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class sumofstringnums {
+    // Function to find sum of all possible substrings of the given string.
+    public static long sumSubstring(String s) {
+        long sum = 0;
+        long mul = 1;
+        long mod = 1000000007;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            sum = (sum + (s.charAt(i) - '0') * mul * (i + 1)) % mod;
+            mul = (mul * 10 + 1) % mod;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter the string
+        System.out.print("Enter the string: ");
+        String s = scanner.next();
+
+        // Calculate the sum of all possible substrings of the given string
+        long result = sumSubstring(s);
+
+        // Print the result
+        System.out.println("Sum of all possible substrings of the given string: " + result);
+
+        // Close the scanner
+        scanner.close();
+    }
+}
