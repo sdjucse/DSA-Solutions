9dc5db8ae681a5d90266019c185d285b8bb75351
Sarthak Dubey <sarthak.dubey1208@gmail.com>
2025-02-12 17:07:48 +0530

    Create LRUCache.java

---
+++ DataStructure Design/LRUCache.java
@@ -0,0 +1,31 @@
+
+import java.util.*;
+
+
+public class LRUCache{
+    private LinkedHashMap<Integer,Integer>map= new LinkedHashMap<>();
+    private int capacity;
+
+    public LRUCache(int capacity){
+        this.capacity=capacity;
+    }
+
+    public int get(int key){
+        if(!map.containsKey(key)){
+            return -1;
+        }
+        Integer val=map.get(key);
+        put(key,val);
+        return val;
+
+    }
+    public void put(int key,int value){
+        if(map.containsKey(key)){
+            map.remove(key);
+        }
+        else if(capacity==map.size()){
+            map.remove(map.keySet().iterator().next());
+        }
+        map.put(key, value);
+    }
+}
