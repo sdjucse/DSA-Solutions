e041a9b9f8b70fb47d5bb08554759ac6a449a539
Sarthak Dubey <sarthak.dubey1208@gmail.com>
2025-02-26 18:21:53 +0530

    Create FindUniqueBinaryString.java

---
+++ leetcodepotd/FindUniqueBinaryString.java
@@ -0,0 +1,17 @@
+
+
+public class FindUniqueBinaryString{
+    public String findDifferentBinaryString(String[] nums) {
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<nums.length;i++){
+            if(nums[i].charAt(i)=='0'){
+                sb.append('1');
+            }
+            else{
+                sb.append('0');
+            }
+        }
+        return sb.toString();
+
+    }
+}
