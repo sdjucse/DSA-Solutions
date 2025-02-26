ea774e583633340c4951912d9c81242f1f21679a
Sarthak Dubey <sarthak.dubey1208@gmail.com>
2025-02-26 18:22:29 +0530

    Update ProductOfLastKNumbers.java

--- Arrays/ProductOfLastKNumbers.java
+++ Arrays/ProductOfLastKNumbers.java
@@ -20,4 +20,32 @@ public class ProductOfLastKNumbers{
         }
         return prod;
     }
+    //Approach-2 (Follow up optimal in O(1))
+//T.C : O(1) for both methods
+//S.C : O(n) for the stream
+private List<Integer> nums; // cumulative prod store karenge
+
+public void add1(int num) {
+    if(num == 0) {
+        nums.clear();
+        n = 0;
+    } else {
+        if(nums.isEmpty()) {
+            nums.add(num);
+        } else {
+            nums.add(nums.get(n-1) * num);
+        }
+        n++; // increasing size by 1
+    }
+}
+
+public int getProduct1(int k) {
+    if(k > n) {
+        return 0;
+    } else if(k == n) {
+        return nums.get(n-1);
+    }
+
+    return nums.get(n-1) / nums.get(n-k-1);
+}
 }
