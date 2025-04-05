d4e3a0a8ebb3c94ea5b58f2bfa3d2a9a9ecaacc1
Sarthak Dubey <65351220+sdjucse@users.noreply.github.com>
2025-04-05 23:56:24 +0530

    Update FindUnion.java

--- Arrays/FindUnion.java
+++ Arrays/FindUnion.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.HashSet;
 
 public class FindUnion {
+    /*
+     * Time Complexity: O((N+M)log(M+N)) where N is the length of arr1 and M is the length of arr2.
+     * Space Complexity: O(N+M) for the HashSet and ArrayList.
+     */
     static ArrayList<Integer>FindUnion(int arr1[],int arr2[],int n,int m){
         HashSet<Integer>set=new HashSet<>();
         ArrayList<Integer>list=new ArrayList<>();
@@ -18,4 +22,34 @@ public class FindUnion {
         }
         return list;
     }
-}
+    /*
+     * Approach-2: Using Two Pointers
+     * Time Complexity: O(N+M) where N is the length of arr1 and M is the length of arr2.
+     *  Space Complexity: O(N+M) for the two pointers.
+     */
+    static ArrayList<Integer> FindUnion2(int arr1[],int arr2[],int n,int m){
+         ArrayList<Integer>list=new ArrayList<>();
+         int i=0,j=0;
+            while(i<n && j<m){
+                if(arr1[i]<arr2[j]){
+                    if(list.size()==0 || list.get(list.size()-1)!=arr1[i]){
+                        list.add(arr1[i]);
+                    }
+                    i++;
+                }else if(arr1[i]>arr2[j]){
+                    if(list.size()==0 || list.get(list.size()-1)!=arr2[j]){
+                        list.add(arr2[j]);
+                    }
+                    j++;
+                }else{
+                    if(list.size()==0 || list.get(list.size()-1)!=arr1[i]){
+                        list.add(arr1[i]);
+                    }
+                    i++;
+                    j++;
+                }
+            }
+            return list;
+        }
+    }
+
