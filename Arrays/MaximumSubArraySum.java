d173d036d7a7c8e4417ff26f0fd80cd81e3fb941
Sarthak Dubey <65351220+sdjucse@users.noreply.github.com>
2025-04-06 12:58:06 +0530

    Update MaximumSubArraySum.java

--- Arrays/MaximumSubArraySum.java
+++ Arrays/MaximumSubArraySum.java
@@ -1,5 +1,17 @@
 
 public class MaximumSubArraySum {
+    /*
+     * Approach:
+     * 1. Initialize a variable `maxi` to Long.MIN_VALUE to keep track of the maximum sum.
+     * 2. Initialize a variable `sum` to 0 to keep track of the current sum.
+     * 3. Iterate through the array:
+     *   - Add the current element to `sum`.
+     *  - If `sum` is greater than `maxi`, update `maxi` to `sum`.
+     *  - If `sum` becomes negative, reset it to 0.
+     * 4. Return `maxi` as the result.
+     *  * Time Complexity: O(N) where N is the length of the array.
+     * Space Complexity: O(1) for the sum variable.
+     */
     public static long maxSubarraySum(int[] arr, int n) {
         long maxi = Long.MIN_VALUE; // maximum sum
         long sum = 0;
@@ -10,7 +22,7 @@ public class MaximumSubArraySum {
 
             if (sum > maxi) {
                 maxi = sum;
-            }
+            }   
 
             // If sum < 0: discard the sum calculated
             if (sum < 0) {
