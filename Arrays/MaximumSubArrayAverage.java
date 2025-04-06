372182ab53870856547c0df178bc0aad2e43e141
Sarthak Dubey <65351220+sdjucse@users.noreply.github.com>
2025-04-06 13:27:05 +0530

    Create MaximumSubArrayAverage.java

---
+++ Arrays/MaximumSubArrayAverage.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+public class MaximumSubArrayAverage {
+    public double findMaxAverage(int[] nums, int k) {
+        int  n=nums.length;
+        double max_avg=Integer.MIN_VALUE;
+        int l=0;
+        int r=k;
+        int sum=0;
+        for(int i=0;i<k;i++){
+            sum+=nums[i];
+        }
+        int max_sum=sum;
+        while(r<n){
+            sum-=nums[l];
+            l++;
+            sum+=nums[r];
+            r++;
+            max_sum=Math.max(sum,max_sum);
+        }
+        return (double)max_sum/k;
+    }
+}
