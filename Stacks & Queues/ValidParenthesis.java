4554930fb5a62d98c9266eb20da3463ecea0b53d
Sarthak Dubey <65351220+sdjucse@users.noreply.github.com>
2025-04-07 13:23:00 +0530

    Create ValidParenthesis.java

---
+++ Stacks & Queues/ValidParenthesis.java
@@ -0,0 +1,26 @@
+
+import java.util.Stack;
+
+public class ValidParenthesis {
+    public static boolean ValidParenthesisCheck(String s){
+        Stack<Character> stack = new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(ch=='('||ch=='{'||ch=='['){
+                stack.push(ch);
+            }
+            else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char top=stack.peek();
+                if((top=='('&&ch==')')||(top=='{'&&ch=='}')||(top=='['&&ch==']')){
+                    stack.pop();
+                }
+                else{
+                    return false;
+                }
+                
+            }
+        }
+    }
+}
