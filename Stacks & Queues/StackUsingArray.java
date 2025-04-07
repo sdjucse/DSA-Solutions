30f878ff79cee7bf9e7df19415c69456751c7a48
Sarthak Dubey <65351220+sdjucse@users.noreply.github.com>
2025-04-07 13:28:58 +0530

    Create StackUsingArray.java

---
+++ Stacks & Queues/StackUsingArray.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class StackUsingArray {
+    Scanner scanner = new Scanner(System.in);
+    int size = scanner.nextInt(); // Size of the stack
+    int arr[] = new int[size];
+    int top = -1;
+    void push(int x) {
+        top++;
+        arr[top] = x;
+    }
+    int pop() {
+        int x = arr[top];
+        top--;
+        return x;
+    }
+    int top() {
+        return arr[top];
+    }
+    int size() {
+        return top + 1;
+    }
+}
