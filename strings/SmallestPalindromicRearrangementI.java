b0a16502c1087b604cc33edaaddb7c7a680c187f
Sarthak Dubey <65351220+sdjucse@users.noreply.github.com>
2025-04-18 12:08:24 +0530

    Create SmallestPalindromicRearrangementI.java

---
+++ strings/SmallestPalindromicRearrangementI.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class SmallestPalindromicRearrangementI {
+    public String smallestPalindrome(String s) {
+        int n = s.length();
+        int m = n / 2;
+
+        if (n == 1 || n == 2) return s;
+
+        char[] fArr = s.substring(0, m).toCharArray();
+        Arrays.sort(fArr);
+        String f = new String(fArr);
+
+        StringBuilder rev = new StringBuilder(f).reverse();
+
+        if (n % 2 == 1) {
+            f += s.charAt(m);
+        }
+
+        return f + rev.toString();
+      }
+}
