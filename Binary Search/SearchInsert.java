0fd502a17468f1773250a9fc19f22983a12e3bd1
Sarthak Dubey <65351220+sdjucse@users.noreply.github.com>
2025-04-19 15:15:48 +0530

    Create SearchInsert.java

---
+++ Binary Search/SearchInsert.java
@@ -0,0 +1,21 @@
+public class SearchInsert {
+    public int searchInsert(int[]nums,int target){
+        int n=nums.length;
+        int left=0,right=n-1;
+        
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]<=target){
+                left=mid+1;
+            }
+            else if(nums[mid]==target){
+                return mid;
+            }
+            else{
+                right=mid-1;
+            }
+
+        }
+        return left;
+    }
+}
